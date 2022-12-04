beca13b0701ff7cd042be98f863654089d83bc91
Russell Matney <russell.matney@gmail.com>
2022-12-03 19:29:08 -0500

    feat: parsing input file content

--- src/2022/01/CalorieCounting.gd
+++ src/2022/01/CalorieCounting.gd
@@ -1,11 +1,32 @@
 tool
 extends Node2D
 
+func file_content(fname):
+	var fpath = str("res://src/2022/01/", fname)
 
-func elf_inventories(f = "input.txt"):
-	f = str("res://src/2022/01/", f)
-	print(f)
-	return f
+	var f = File.new()
+
+	if not f.file_exists(fpath):
+		print("[WARN] file does not exist: ", fpath)
+		return
+	else:
+		print("[OK] file exists: ", fpath)
+
+	f.open(fpath, File.READ)
+	var content = f.get_as_text()
+	f.close()
+
+	return content
+
+
+func elf_inventories(fname = "input.txt"):
+	var content = file_content(fname)
+	print(content)
+	return content
 
 func _ready():
+	if Engine.editor_hint:
+		request_ready()
+
+	# elf_inventories("input.txt")
 	elf_inventories("example.txt")
