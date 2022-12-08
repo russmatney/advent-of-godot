72c5a3b8e2bc0358bfc26a4151f42b693441157b
Russell Matney <russell.matney@gmail.com>
2022-12-08 13:07:18 -0500

    day 1: adding elves with inventories as rigid bodies

---
+++ src/2022/01/Elf.gd
@@ -0,0 +1,11 @@
+tool
+extends RigidBody2D
+
+var inventory = []
+
+func _ready():
+	pass # Replace with function body.
+
+
+func assign_inventory(inv):
+	inventory = inv
---
+++ src/2022/01/OnTheBoat.gd
@@ -0,0 +1,37 @@
+tool
+extends Node2D
+
+var elves = []
+
+var parsed_inventories = []
+var data_fname = "input.txt"
+
+######################################################
+
+func _ready():
+	if Engine.editor_hint:
+		request_ready()
+
+		# use the example data in the editor
+		data_fname = "example.txt"
+
+	parsed_inventories = $CalorieCounting.elf_inventories(data_fname)
+
+	spawn_elves(parsed_inventories)
+
+######################################################
+
+func spawn_elves(inventories):
+	for inv in inventories:
+		var elf = create_elf(inv)
+		elves.append(elf)
+		$ElfContainer.add_child(elf)
+
+######################################################
+
+var elf_scene = preload("res://src/2022/01/Elf.tscn")
+
+func create_elf(inventory):
+	var inst = elf_scene.instance()
+	inst.assign_inventory(inventory)
+	return inst
