c4e30eaabc7abcf8eaded71e50710566287045b7
Russell Matney <russell.matney@gmail.com>
2022-12-08 15:54:02 -0500

    feat: camera zoom now also reacting to screen/window resizing

--- src/2022/01/Camera2D.gd
+++ src/2022/01/Camera2D.gd
@@ -3,19 +3,24 @@ extends Camera2D
 export (String) var follow_group
 
 var to_follow = []
+var zoom_offset = Vector2(100, 100)
+var window_size = OS.window_size
 
 func _ready():
 	if follow_group:
 		update_follows()
 	else:
 		print("[WARN][CAMERA]: no follow_group indicated")
+	update_window_size()
+
+	var _x = get_tree().connect("screen_resized", self, "update_window_size")
+
+func update_window_size():
+	window_size = OS.window_size
 
 func update_follows():
 	if follow_group:
 		to_follow = get_tree().get_nodes_in_group(follow_group)
-
-var zoom_offset = Vector2(100, 100)
-var screen_res = Vector2(1920, 1080)
 
 func _process(_delta):
 	if not to_follow:
@@ -53,8 +58,8 @@ func _process(_delta):
 		center = center / to_follow.size()
 		self.global_position = center
 
-		var zoom_factor1 = abs(max_right - max_left) / (screen_res.x - zoom_offset.x)
-		var zoom_factor2 = abs(max_bottom - max_top) / (screen_res.y - zoom_offset.y)
+		var zoom_factor1 = abs(max_right - max_left) / (window_size.x - zoom_offset.x)
+		var zoom_factor2 = abs(max_bottom - max_top) / (window_size.y - zoom_offset.y)
 		var zoom_factor = max(max(zoom_factor1, zoom_factor2), 0.5)
 
 		self.zoom = Vector2(zoom_factor, zoom_factor)
