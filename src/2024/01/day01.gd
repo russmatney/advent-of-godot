aca9a98e5a812e86b662785a88b12c8237db3b0c
Russell Matney <russell.matney@gmail.com>
2024-12-06 14:56:40 -0500

    feat: 2024 day 01 part 1 complete

--- src/2024/01/day01.gd
+++ src/2024/01/day01.gd
@@ -1,9 +1,28 @@
 extends Object
 class_name AOC2024Day01
 
-
-
 static func input(fname):
 	var content = Util.file_content(str("res://src/2024/01/", fname, ".txt"))
-	Log.pr(content)
-	return content
+	var lines = content.split("\n", false)
+	var left = []
+	var right = []
+	for line in lines:
+		var floats = line.split_floats("   ")
+		left.append(int(floats[0]))
+		right.append(int(floats[1]))
+	return {left=left, right=right}
+
+static func calc_part_one(data):
+	var left = data.left
+	var right = data.right
+
+	# sort the lists
+	left.sort()
+	right.sort()
+
+	# calc the difference
+	var dist = 0
+	for i in range(len(left)):
+		dist += abs(left[i] - right[i])
+
+	return dist
--- test/unit/2024/day01_test.gd
+++ test/unit/2024/day01_test.gd
@@ -2,6 +2,15 @@ extends GdUnitTestSuite
 
 func test_input_example():
 	var data = AOC2024Day01.input("example")
+	assert_that(data.left).is_equal([3, 4, 2, 1, 3, 3])
+	assert_that(data.right).is_equal([4, 3, 5, 3, 9, 3])
 
-	assert_that(data.left).is_equal([3, 4, 2, 1])
-	assert_that(data.right).is_equal([4, 3, 5])
+func test_part_one_example():
+	var data = AOC2024Day01.input("example")
+	var result = AOC2024Day01.calc_part_one(data)
+	assert_that(result).is_equal(11)
+
+func test_part_one_input():
+	var data = AOC2024Day01.input("input")
+	var result = AOC2024Day01.calc_part_one(data)
+	assert_that(result).is_equal(1666427)
